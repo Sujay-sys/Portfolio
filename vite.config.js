f05e99ea2e4a42305804d764d1f155d7219c8ba2
Sujay-sys <sujayacharya189@gmail.com>
2026-06-02 01:01:58 +0530

    fix: set base path to root for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portofolio/",
+  base: "/",  // <--- Changed this to a single slash
   plugins: [react(), tailwindcss()],
 })
